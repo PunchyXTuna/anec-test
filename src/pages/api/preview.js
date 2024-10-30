c028ef663e71d200a16e71002b614d86af4fa9a2
Yacin Bouraoui <143107498+PunchyXTuna@users.noreply.github.com>
2024-10-30 18:51:56 +0100

    Add files via upload

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -3,9 +3,7 @@ import { previewClient } from "contentful";
 const handler = async (req, res) => {
   const { secret, slug } = req.query;
 
-  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !slug) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+
 
   const response = await previewClient.getEntries({
     content_type: "post",
